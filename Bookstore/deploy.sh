c6a1d819884fd2acba1ea7c9d69484d444a0ccad
Jasper Hanlon <jasper.hanlon@mohawkcollege.ca>
2021-08-29 21:53:35 -0400

    Update project deploy script, reduce published file size

--- Bookstore/deploy.sh
+++ Bookstore/deploy.sh
@@ -6,7 +6,10 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 DB="$SCRIPTPATH/bookstore.prod.db"
 [ -e "$DB" ] && rm "$DB"
 
-dotnet publish --configuration Release
-ASPNETCORE_ENVIRONMENT=Production ./remigrate.sh
-cp "$DB" $SCRIPTPATH/bin/Release/net5.0/publish/
-tar -C bin/Release/net5.0/publish/ -cvf $SCRIPTPATH/bookstore.tar.gz .
+PUBLISHDIR="$SCRIPTPATH/bin/Release/net5.0/linux-x64/publish/"
+
+dotnet clean
+dotnet publish -r linux-x64 --no-self-contained --configuration Release
+ASPNETCORE_ENVIRONMENT=Production dotnet-ef database update -v
+cp "$DB" "$PUBLISHDIR"
+tar -C "$PUBLISHDIR" -cvf $SCRIPTPATH/bookstore.tar.gz .
